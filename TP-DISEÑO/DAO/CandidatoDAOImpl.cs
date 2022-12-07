5665930518b0ebd175d942d3c4d772581f5c90c3
EstefanoCozzi <estefanocozzi@gmail.com>
2022-12-07 14:41:25 -0300

    cdu 001

---
+++ TP-DISEÑO/DAO/CandidatoDAOImpl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using TP_DISEÑO;
+using TP_DISEÑO.DTO;
+
+namespace TP_DISEÑO.DAO
+{
+    class CandidatoDAOImpl : ICandidatoDAO
+    {
+        public candidato GetCandidatoByDoc(int numDoc, string TipoDoc, CapitalHumanoEntities context)
+        {
+            candidato candidato = null;
+            try
+            {
+                candidato = context.candidato.Where(c => c.numDoc == numDoc & c.TipoDoc == TipoDoc).FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caught.", e);
+            }
+            return candidato;
+        }
+
+        public cuestionario GetUltimoCuestionarioActivo(int numDoc, CapitalHumanoEntities context)
+        {
+
+            cuestionario cuestionario = null;
+
+            try
+            {
+                (context.candidato.Where(c => c.numDoc == numDoc).FirstOrDefault()).cuestionario.Estado = "Activo";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caught.", e);
+            }
+            return cuestionario;
+
+        }
+
+    }
+
+}
---
+++ TP-DISEÑO/DTO/UsuarioDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_DISEÑO.DTO
+{
+    internal class UsuarioDTO
+    {
+
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string nombreUsuario { get; set; }
+        public string contraseña { get; set; }
+        public string numDocumento { get; set; }
+        public string Tipo { get; set; }
+
+    }
+}
