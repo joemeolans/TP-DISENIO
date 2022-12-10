2c4aac44184ebc6a1c117355c7c9db2635ddcb84
joemeolans <72466165+joemeolans@users.noreply.github.com>
2022-12-10 20:53:38 -0300

    Hice la maqueta del CDU026

---
+++ TP-DISEÑO/Interfaz/CDU026-Maqueta1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_DISEÑO.Interfaz
+{
+    public partial class CDU026_Maqueta1 : Form
+    {
+        public CDU026_Maqueta1()
+        {
+            InitializeComponent();
+        }
+
+        private void botonVolver_Click(object sender, EventArgs e)
+        {
+            Form vueltaAlMenu = new MenuPrincipal();
+            vueltaAlMenu.Show();
+            this.Hide();
+        }
+
+        private void CerrarSesion_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Desea Salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == System.Windows.Forms.DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
